f81ae28ea2363e8c2dde73278665712e32f757b6
miftakhair <89115969+miftakha@users.noreply.github.com>
2024-05-30 09:25:31 +0700

    Revert "Merge remote-tracking branch 'origin/NewBranchTugasAkhir'"
    This reverts commit 7f6ec07024e3e1a61d99fe43e308a5cf3a4b97cc, reversing
    changes made to c409c6f91f5bc8592ca7771967d29b57519b1c25.

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -2,18 +2,14 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    private GameManager gameManager;
-
-    private void Start()
-    {
-        gameManager = FindObjectOfType<GameManager>();
-    }
+    public GameManager gameManager;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player1") || other.CompareTag("Player2"))
+        if (other.CompareTag("Player"))
         {
-            gameManager.CollectCoin();
+            gameManager.CoinCollected();
+            // Coin ilang setelah diambil
             Destroy(gameObject);
         }
     }
