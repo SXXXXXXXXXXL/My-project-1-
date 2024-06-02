85321271542e51378452a6120053279f9ea01f1a
miftakhair <89115969+miftakha@users.noreply.github.com>
2024-06-02 14:59:29 +0700

    update sound fx

---
+++ Assets/Miftahul/script/SoundEffectsManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFXManager : MonoBehaviour
+{
+    public static SoundFXManager instance;
+
+    [SerializeField] private AudioSource soundFXObject;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        //spawn in game object
+        AudioSource audioSource = instantiate()
+
+        //assign the adioclip
+
+        //assign volume
+
+        //play sound
+
+        //get length of clip
+
+        //destroy the clip after it is done playing
+    }
+}
