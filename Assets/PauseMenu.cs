da260cdc6172a902d6bd8c183e716f9b1f691957
rizqullahmubarok <mubarokrizqullah@gmail.com>
2024-06-08 12:37:29 +0700

    memberbaiki menu pause dan tutorial

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -4,31 +4,29 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject tutorialMenu;
 
     public static bool isPaused = false;
 
-    // Pause the game and show the pause menu
-    public void Pause()
+    // Toggle pause state and show/hide the pause menu
+    public void TogglePause()
     {
-        if (!isPaused)
+        if (isPaused)
         {
-            Time.timeScale = 0f; // Pause the game
-            pauseMenu.SetActive(true);
-            isPaused = true;
+            Resume();
         }
         else
         {
-            Time.timeScale = 1f; // Pause the game
-            pauseMenu.SetActive(true);
-            isPaused = false;
+            Pause();
         }
     }
 
-    // Return to the main menu
-    public void Home()
+    // Pause the game and show the pause menu
+    private void Pause()
     {
-        Time.timeScale = 1f; // Unpause the game before leaving the scene
-        SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 0f; // Pause the game
+        pauseMenu.SetActive(true);
+        isPaused = true;
     }
 
     // Resume the game and hide the pause menu
@@ -39,10 +37,32 @@ public class PauseMenu : MonoBehaviour
         isPaused = false;
     }
 
+    // Return to the main menu
+    public void Home()
+    {
+        Time.timeScale = 1f; // Unpause the game before leaving the scene
+        SceneManager.LoadScene("Main Menu");
+    }
+
     // Restart the current scene
     public void Restart()
     {
         Time.timeScale = 1f; // Unpause the game before restarting the scene
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
+
+    // Show the tutorial menu and pause the game
+    public void ShowTutorial()
+    {
+        Time.timeScale = 0f; // Pause the game
+        tutorialMenu.SetActive(true);
+    }
+
+    // Hide the tutorial menu and resume the game
+    public void HideTutorial()
+    {
+        Time.timeScale = 1f; // Unpause the game
+        tutorialMenu.SetActive(false);
+    }
 }
+
